389e5b52d167707cac36b30b2348e55846b57c51
mckennahpalmer <mkpalm2006@gmail.com>
2022-09-13 14:16:33 -0600

    Adding the Routes folder

--- name.js
+++ name.js
@@ -1,14 +1,18 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const app = express();
 const port = 3000;
 
+const hostname = '127.0.0.1';
+
+app.use('/', require('./routes'))
+
+/*
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/fancy');
   res.end('Mckennah Palmer');
-});
+});*/
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, hostname, () => {
+  console.log(`App listening at http://${hostname}:${port}/`);
 });
