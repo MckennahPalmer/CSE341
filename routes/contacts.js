f76cac43c3bde472589f0e53e1d5c3a86825278c
mckennahpalmer <mkpalm2006@gmail.com>
2022-10-01 12:24:23 -0600

    POST, PUT, DELETE Routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -9,4 +9,10 @@ router.get('/:id', getSingle);
 
 router.get('/oid/:oid', getSingle);
 
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
+
 module.exports = router;
